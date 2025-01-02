cc5413e0ed87e48dd08af661434969a700d4735b
Hadil Yahi <165954236+hadilyahi@users.noreply.github.com>
2025-01-02 23:50:12 +0100

    delete the folders of output ++ Sorting Algorithms

---
+++ SerieProblems/SORTINGProblems.c
@@ -0,0 +1,41 @@
+// SORTING
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int sotingSection(int arr[] , int N){
+   for (int i =0 ; i<N ; i++){
+    int min = i;
+    for(int j = i+1 ; j<N ; j++){
+        if(arr[j]<arr[min]){
+            min = j;
+        }
+    }
+    int temp = arr[min];
+    arr[min]=arr[i];
+    arr[i]=temp;
+    
+   }
+}
+int main(){
+    int N;
+    printf("Enter the size of the array : ");
+    scanf("%d",&N);
+    int arr[N];
+    for(int i = 0 ; i<N ; i++){
+        printf("Enter number : ");
+        scanf("%d",&arr[i]);
+    }
+    printf("the array before sorting : ");
+    for(int i = 0 ; i<N ; i++){
+        printf("%d",arr[i]);
+        printf(" , ");
+    }
+    sotingSection(arr,N);
+    printf("\n the array after sorting : ");
+    for(int i = 0 ; i<N ; i++){
+        printf("%d",arr[i]);
+        printf(" , ");
+    }
+    return 0;
+}
